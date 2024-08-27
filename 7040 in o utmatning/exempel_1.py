5301dcea5124cff140115e9410550a02088de247
Elias Berntsson <elias.berntsson@outlook.com>
2024-08-27 16:14:22 +0200

    python

--- 7040 in o utmatning/exempel_1.py
+++ 7040 in o utmatning/exempel_1.py
@@ -1,4 +1,4 @@
 namn = input("Vad heter du?")
 färg = input("Vad är din favoritfärg?")
-print("Hej", namn, ",", färg, "är verkligen en cool färg!")
+print("Hej", namn+ ",", färg,"är verkligen en cool färg!")
 print("Har du någonsin? Tar det som ett nej...")
