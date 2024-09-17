209b3acb5400b62cb5d9c6e487afa190d5385327
Elias Berntsson <elias.berntsson@outlook.com>
2024-09-17 13:54:10 +0200

    några ändringar

--- 7060 while/fibonacci.py
+++ 7060 while/fibonacci.py
@@ -1,8 +1,15 @@
+#fick inte isinstance att funka som jag ville. Ville att Programmet skulle funka även om användaren matar in något annat än ett heltal.
+
 A = 1
 B = 1
 N = 1
 antal = int(input("Ange antalet tal i Fibonacci talföljden som ska räknas ut: "))
-print("Räknar ut", antal, "tal")
+if isinstance(antal, int):
+    antal = int(antal)
+    print("Räknar ut", str(antal), "tal")
+else:
+    antal = 30
+    print("Kunde ej läsa värde, räknar ut 30st tal")
 print("Tal nummer", str(N) + ":", A)
 N = N + 1
 print("Tal nummer", str(N) + ":", B)
