c42b1d0b85e1480da56227433c56b2b8727d17e8
Elias Berntsson <elias.berntsson@outlook.com>
2024-11-11 12:03:52 +0100

    bolibompa

---
+++ bolibia.py
@@ -0,0 +1,6 @@
+max = 13
+
+bakelser = ["m1", "m1", "m2", "k6", "k7", "c3"]
+for tal in bakelser:
+    if tal == 1:
+        print("hej")
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-you = "you"
 
-if isinstance(you, str) == True:
-    print("det stämemr")
-else:
-    print("stimm icke")
+lista = ["block 1", 2, "block 3"]
+
+
+
+
+# you = "you"
+
+# if isinstance(you, str) == True:
+#     print("det stämemr")
+# else:
+#     print("stimm icke")
