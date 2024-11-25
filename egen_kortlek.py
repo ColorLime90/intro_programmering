e3f4bd357cff8ec80175eb726409e83d2e2045e3
Elias Berntsson <elias.berntsson@outlook.com>
2024-11-25 13:08:34 +0100

    kortlek

--- egen_kortlek.py
+++ egen_kortlek.py
@@ -1,15 +1,42 @@
-#Junk
-
 import random
-kortlek_bas = [H1, ]
+kortlek_bas = []
+antal_kortlekar = ""
+kortlek = []
+
+for l in ("HDSC"):
+    for n in range(1, 14):
+        kortlek_bas.append(str(l) + str(n))
+print(kortlek_bas)
+
+antal_kortlekar = int(input("Hur många kortlekar vill du spela med? "))
+
+for i in range(1, antal_kortlekar + 1):
+    kortlek.extend(kortlek_bas)
+
+print(kortlek)
+
+def count_amount(x, y):
+    count_amount_temp = 0
+    for i in y:
+        if i == x:
+            count_amount_temp = count_amount_temp + 1
+    return count_amount_temp
 
 
-antal_kortlekar = input("Hur många kortlekar vill du spela med?")
+# By default, a module has some hidden variables defined
+# print({k: v for k, v in globals().items() if not k.startswith("__")})
+#
+# for i in range(1, 11):
+#    globals()[f"my_variable_{i}"] = i
+#
+# print()
+# print(my_variable_1)
+# print(my_variable_2)
+# and so on
+#
+# print()
+# print({k: v for k, v in globals().items() if not k.startswith("__")})
 
-for x in range(0, 4):
-    def dra_kort():
-        hjärta = random.randrange(1, 13, 1)
-    dra_kort()
-print(hjärta)
-
-
+def skapa_spelare(x):
+    for i in range(1, x + 1):
+       spelare = []
